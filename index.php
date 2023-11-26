30509f4f3953ca8dbe0092ba47c6da4aec17762b
anonimoDoGithub <xxexpgamerxx@gmail.com>
2023-11-26 11:01:40 -0300

    adicionando operador ternário

--- index.php
+++ index.php
@@ -3,7 +3,13 @@
 $idade = 40;
 
 if($idade < 18) {
-    echo 'Você é maior de idade!';
+    echo 'Você é maior de idade!'."<br/>";
 }else {
-    echo 'Você é menor de idade!';
-}
+    echo 'Você é menor de idade!'."<br/>";
+};
+
+// Operador Ternário
+$numero = 2 % 2 === 0 ? "O número é par!" : "O número é impar!" ;
+echo $numero."<br/>";
+$numero = 3 % 2 === 0 ? "O número é par!" : "O número é impar!" ;
+print_r($numero)."<br/>";
